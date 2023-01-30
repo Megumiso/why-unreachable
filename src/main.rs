78c25fadad63ba59425ba531ba2593ac3491a80c
Megumiso <github@megumiso.com>
2023-01-30 06:38:59 +0000

    more code

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,15 @@
 async fn main() {
     println!("Hello, world!");
     let client = reqwest::Client::new();
-    let _hoge = client.post("http://example.com")
+    let hoge  = client.get("http://example.com") //Unused???
         .send()
         .await
-        .expect({
+        .expect({ // Unreachable??
             std::process::exit(1);
-        });
+        })
+        .text()
+        .await
+        .expect("convert to text is failed somehow!");
+    println!("Reached!: Here's the output: {}", hoge);
     std::process::exit(0);
 }
